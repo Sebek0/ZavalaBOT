511ad9d250b6b76771a3b11e670317d39ccf116c
Sebek0 <anaruk6@gmail.com>
2022-09-03 14:32:32 +0200

    Add manifest validation task

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import logging
 
@@ -5,7 +6,7 @@ import aiohttp
 import discord
 import yaml
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from custom_logging import CustomFormatter
 from bungie_api_wrapper.manifest import Manifest
@@ -32,15 +33,24 @@ class Bot(commands.Bot):
     def __init__(self) -> None:
         intents = discord.Intents.all()
         allowed_mentions = discord.AllowedMentions.all()
-        if not config['development']:
-            self.manifest = Manifest()
         super().__init__(command_prefix='.', allowed_mentions=allowed_mentions,
                          intents=intents)
+        
+    async def setup_hook(self) -> None:
+        logger.debug('About to start loading extenstions...')
+        try:
+            await self.load_extension('discord_bot.cogs.commands')
+            if config['server_logging']:
+                await self.load_extension('discord_bot.cogs.listeners')
+            if not config['development']:
+                self.bg_task = self.loop.create_task(self.check_bungie_manifest_version())
+            logger.info('Loaded extensions.')
+        except discord.DiscordException as discord_error:
+            logger.error('{}'.format(discord_error))
     
     async def on_ready(self) -> None:
         logger.debug('About to start {}...'.format(self.user))
         
-        await self.wait_until_ready()
         try:
             await bot.tree.sync(guild=discord.Object(id=guild_id))
             logger.info('Synced application commands with Discord.')
@@ -49,23 +59,18 @@ class Bot(commands.Bot):
         except discord.Forbidden:
             logger.error('The client does not have the applications.commands'
                               'scope in the guild.')
-        
-        # Checking if Bungie API manifest is up to date with bot startup.
-        if not config['development']:
-            self.manifest.check_manifest()
             
         logger.info('Bot logged in as {}'.format(self.user))
         logger.info('{} is ready to use.'.format(self.user))
-        
-    async def setup_hook(self) -> None:
-        logger.debug('About to start loading extenstions...')
-        try:
-            await self.load_extension('discord_bot.cogs.commands')
-            await self.load_extension('discord_bot.cogs.listeners')
-            logger.info('Loaded extensions.')
-        except discord.DiscordException as discord_error:
-            logger.error('{}'.format(discord_error))
-              
+    
+    # Check if Bungie API manifest is up to date with bot startup.   
+    async def check_bungie_manifest_version(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            self.manifest.check_manifest()
+            await asyncio.sleep(86400)
+            
+    
 bot = Bot()
 
 def main():
